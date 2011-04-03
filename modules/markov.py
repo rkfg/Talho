07df5d00144a5c84e706423b7efc5ffc71df1135
eurekafag <eurekafag@eureka7.ru>
2011-04-03 18:54:42 +0400

    IQ increased.

--- modules/markov.py
+++ modules/markov.py
@@ -15,7 +15,7 @@ class Markov():
         
         self.conn = sqlite3.connect(dbname)
         self.c = self.conn.cursor()
-        self.c.execute('CREATE TABLE IF NOT EXISTS words(id integer primary key autoincrement, word1 text, word2 text, word3 text)')
+        self.c.execute('CREATE TABLE IF NOT EXISTS words(word1 text, word2 text, word3 text)')
         self.c.execute('CREATE INDEX IF NOT EXISTS mainindex on words(word1, word2, word3)')
         self.tablesize = self.c.execute("SELECT COUNT(*) FROM words").fetchone()[0]
 
@@ -30,13 +30,20 @@ class Markov():
 
     def build_chain(self, length):
         """Returns string of required words count"""
-        
+
+        if length == 1:
+            words = self.c.execute("SELECT word1, word2, word3 FROM words WHERE word3 LIKE '%.' OR word3 LIKE '%?' OR word3 LIKE '%!' ORDER BY RANDOM() LIMIT 1").fetchone()
+            return ' '.join(words)
+            
         words = self.c.execute("SELECT word1, word2, word3 FROM words LIMIT 1 OFFSET ?", (random.randint(0, self.tablesize - 1),)).fetchone()
         result = ' '.join(words)
         for i in xrange(length):
             if not words[2]:
                 break
-            words = self.c.execute("SELECT word1, word2, word3 FROM words WHERE word1=? ORDER BY RANDOM() LIMIT 1", (words[2],)).fetchone()
+            if i != length - 1:
+                words = self.c.execute("SELECT word1, word2, word3 FROM words WHERE word1=? ORDER BY RANDOM() LIMIT 1", (words[2],)).fetchone()
+            else:
+                words = self.c.execute("SELECT word1, word2, word3 FROM words WHERE word1=? AND (word3 LIKE '%.' OR word3 LIKE '%!' OR word3 LIKE '%?') ORDER BY RANDOM() LIMIT 1", (words[2],)).fetchone()
             if not words:
                 break
             result += ' ' + ' '.join(words[1:])
@@ -44,13 +51,17 @@ class Markov():
 
     def add_words(self, string):
         """Adds words to the database"""
+        if not string[-1] in ['.', '!', '?']:
+            string += '.'
         words = string.replace('\n', ' ').split()
         while len(words) < 3:
             words.append("")
             
         for i in xrange(len(words) - 2):
-            self.c.execute("INSERT INTO words (word1, word2, word3) VALUES (?, ?, ?)", (words[i], words[i + 1], words[i + 2]))
-            self.tablesize += 1
+            exists = self.c.execute("SELECT * FROM words WHERE word1=? AND word2=? AND word3=?", (words[i], words[i + 1], words[i + 2])).fetchone()
+            if not exists:
+                self.c.execute("INSERT INTO words (word1, word2, word3) VALUES (?, ?, ?)", (words[i], words[i + 1], words[i + 2]))
+                self.tablesize += 1
         self.conn.commit()
 
 markov = Markov("default.sqlite")
