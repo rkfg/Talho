8e1a03e63c140b9eee1a0e90dd320e3cfdec03b5
eurekafag <eurekafag@eureka7.ru>
2011-05-28 16:02:53 +0400

    mpdproxy upgraded for working with deepshitplayer.

--- mpdproxy.py
+++ mpdproxy.py
@@ -7,9 +7,16 @@ import sys
 client = mpd.MPDClient()
 client.connect("127.0.0.1", "6600")
 play = True
-if sys.argv[1] == '!':
-    play = False
-    del sys.argv[1]
-id = client.addid("http://127.0.0.1:8080/%s" % "+".join(sys.argv[1:]))
-if play:
-    client.playid(id)
+if not sys.stdin.isatty(): # got some data from pipe
+    for track in sys.stdin:
+        track = track.replace(" ", "+").strip()
+        print "Adding %s" % track
+        client.addid("http://127.0.0.1:8080/%s" % track)
+else:
+    if sys.argv[1] == '!':
+        play = False
+        del sys.argv[1]
+    id = client.addid("http://127.0.0.1:8080/%s" % "+".join(sys.argv[1:]))
+    if play:
+        client.playid(id)
+    
