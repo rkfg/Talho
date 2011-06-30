b4c6060e8ada42f790379d04e1360a00325cb1f1
eurekafag <eurekafag@eureka7.ru>
2011-06-30 17:33:21 +0400

    Moved to github.

--- modules/help.py
+++ modules/help.py
@@ -2,7 +2,7 @@ def main(bot, args):
     '''help\nHelp.'''
 
     if not args:
-        return 'Type %lsmod to show avialable modules.\nSources: http://bitbucket.org/eurekafag/talho'
+        return 'Type %lsmod to show avialable modules.\nSources: https://github.com/eurekafag/Talho'
 
 def info(bot):
     return (("help",), 10, main)
