bcc34d4c0de24fa182af63e6b04c6d24630e2d19
eurekafag <eurekafag@eureka7.ru>
2012-07-29 13:59:44 +0400

    vkproxy fixes.

--- vkproxy.py
+++ vkproxy.py
@@ -9,29 +9,15 @@ import traceback
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 from SocketServer import ThreadingMixIn
 import socket
-from vkproxy_settings import vk_acc, icecast_admin
+from vkproxy_settings import vk_acc
 
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
-
-class IcecastAdminOpener(urllib.FancyURLopener):
-    def prompt_user_passwd(self, host, realm):
-        return icecast_admin
-
-def set_tag(args):
-    if not args:
-        return
-    try:
-        song_name = " ".join(args)[:100]
-        metadata_opener = IcecastAdminOpener()
-        update_query = urllib.urlencode({ "mount" : "/radio", "mode" : "updinfo", "song" : song_name })
-        metadata_opener.open("http://127.0.0.1:8000/admin/metadata?" + update_query).read()
-    except:
-        pass
 
 class VkHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         global opener
+
         dl_name = os.tmpnam()
         pathsplit = self.path[1:].split("/")
         songtitle = pathsplit[0]
@@ -41,8 +27,8 @@ class VkHandler(BaseHTTPRequestHandler):
             songshift = 0
 
         try:
-            #result = handle.read().decode("cp1251")
-            req = urllib2.Request("http://vkontakte.ru/search?c%%5Bq%%5D=%s&c%%5Bsection%%5D=audio" % songtitle)
+            req = urllib2.Request("http://vk.com/audio", data="act=search&al=1&gid=0&offset=0&sort=0&q=%s" % urllib.quote_plus(songtitle),
+                                  headers={"X-Requested-With": "XMLHttpRequest", "Referer": "http://vk.com/audio?act=popular"})
 
             handle = opener.open(req)
             result = handle.read()
@@ -52,46 +38,34 @@ class VkHandler(BaseHTTPRequestHandler):
             match = re.findall(ur"value=\"(http:[^,]+),", result)
             if match:
                 link = match[songshift]
-                mp3_handle = urllib2.urlopen(link)
-	        self.send_response(200)
+                print "Link:", link
+                mp3_handle = opener.open(link)
+                
+                self.send_response(200)
                 self.send_header('Content-Type', mp3_handle.headers['Content-Type'])
                 self.send_header('Content-Length', mp3_handle.headers['Content-Length'])
                 self.end_headers()
-	#tmpfile = open(dl_name, "wb")
 
                 print "Transfer started"
-                #if self.client_address[0] == "127.0.0.1":
-                #    set_tag(songtitle.split("+"))
                 while True:
                     chunk = mp3_handle.read(128 * 1024)
                     if not chunk:
                         break
                     self.wfile.write(chunk)
-		    #tmpfile.write(chunk)
-
+                    
                 print "Transfer finished"
-		#tmpfile.close()
                 mp3_handle.close()
-		#tmpfile = open(dl_name, "rb")
-                #while True:
-                #    chunk = tmpfile.read(16 * 1024)
-                #    if not chunk:
-                #        break
-                #    self.wfile.write(chunk)
-		#tmpfile.close()
-		self.wfile.close()
-		#os.unlink(dl_name)
+                self.wfile.close()
                 return
 
-	except socket.error, msg:
+        except socket.error, msg:
             print str("".join(traceback.format_exception(*sys.exc_info())))
-	    #os.unlink(dl_name)
-	    return
+            return
         except:
             print str("".join(traceback.format_exception(*sys.exc_info())))
             print '--------------------------------------------------------'
-	    self.send_response(200)
-	    return
+            self.send_response(404)
+            return
 
         self.send_response(404)
         return
@@ -102,10 +76,13 @@ class VkHandler(BaseHTTPRequestHandler):
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
     pass
 
-url = "http://vkontakte.ru/login.php?email=%s&pass=%s" % vk_acc
-req = urllib2.Request(url)
+url = "http://login.vk.com/?act=login"
+req = urllib2.Request(url, data="email=%s&pass=%s" % vk_acc)
 handle = opener.open(req)
-socket.setdefaulttimeout(30)
+handle.read()
+handle.close()
+print "Ready to serve."
+socket.setdefaulttimeout(15)
 
 server = ThreadedHTTPServer(('0.0.0.0', 8080), VkHandler)
 server.serve_forever()
